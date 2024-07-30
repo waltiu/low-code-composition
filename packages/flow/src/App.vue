5c9c66c56db9239225a9caacab4fa9f5335c1ee0
waltiu <waltiu@163.com>
2024-07-30 09:03:16 +0800

    chore: 删除多余代码

---
+++ packages/flow/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <Diagram ref="editorRef"/>
+  </div>
+</template>
+
+<script>
+import Diagram from './components/Diagram.vue'
+import Vue from 'vue'
+
+export default {
+  name: 'App',
+  components: {
+    Diagram
+  },
+  mounted(){
+    Vue.prototype.$editorRef=this.$refs.$editorRef
+    console.log(this,'editoref',this.$refs.editorRef)
+  },
+  data:()=>{
+    return {
+      editorRef:null
+    }
+  }
+}
+</script>
+
+<style>
+html, body {
+  height: 100%;
+}
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
+<style scoped>
+.container {
+  background-color: #f8f9fa;
+  height: 100%;
+}
+</style>
